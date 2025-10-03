5c3d74c8158d35b3604b51f6c6577f283574db4c
Aditya Pratap Yadav <adityapratap24062004@gmail.com>
2025-10-04 00:02:58 +0530

    Time: 4 ms (85.13%) | Memory: 45.3 MB (46.21%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows <= 1 || s == null || s.length() <= numRows) return s;
+
+        StringBuilder[] rows = new StringBuilder[Math.min(numRows, s.length())];
+        for (int i = 0; i < rows.length; i++) rows[i] = new StringBuilder();
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += (goingDown ? 1 : -1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) result.append(row);
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(new Solution().convert(s, numRows));
+    }
+}
