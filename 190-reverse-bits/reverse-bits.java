8d22f0b0b1b6b28bf16d180a1098d26b34912ea0
Aditya Pratap Yadav <adityapratap24062004@gmail.com>
2026-08-20 22:40:59 +0530

    Time: 1 ms (60.98%) | Memory: 42.5 MB (34.51%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            // Take the last bit of n
+            int bit = n & 1;
+
+            // Shift result left and add the extracted bit
+            result = (result << 1) | bit;
+
+            // Move to the next bit of n
+            n >>>= 1;
+        }
+
+        return result;
+    }
+}
