560f3278654f40588fa3f8204c4d608d5ee0ed12
Aditya Pratap Yadav <adityapratap24062004@gmail.com>
2026-09-18 21:38:00 +0530

    Time: 3 ms (76.97%) | Memory: 42.8 MB (85.51%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < nums.length) {
+            int start = nums[i];
+
+            // Find the end of consecutive numbers
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+
+            int end = nums[i];
+
+            // Single number
+            if (start == end) {
+                result.add(String.valueOf(start));
+            }
+            // Range
+            else {
+                result.add(start + "->" + end);
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
